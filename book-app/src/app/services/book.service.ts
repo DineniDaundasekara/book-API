12cce92007c63bc199772f4cefbd09b04fbb87d8
DineniDaundasekara <daundasekaradineni@gmail.com>
2026-03-13 09:08:37 +0530

    edit files according to data fetching error

--- book-app/src/app/services/book.service.ts
+++ book-app/src/app/services/book.service.ts
@@ -8,7 +8,7 @@ import { Book } from '../models/book.model';
 })
 export class BookService {
 
-  private apiUrl = 'http://localhost:5000/api/books';
+  private apiUrl = 'http://localhost:5227/api/books';
 
   constructor(private http: HttpClient) {}
 
